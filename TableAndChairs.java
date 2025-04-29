240f220332f57760230595b578264ae8f73d5e58
ToamilHere <168946458+Toamil03@users.noreply.github.com>
2025-04-28 22:52:53 -0400

    Add files via upload
    EX08

---
+++ TableAndChairs.java
@@ -0,0 +1,11 @@
+// TableAndChairs.java
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("X     XXXXXXXXXXXX      X");
+        System.out.println("XXXXX X          X XXXXX");
+        System.out.println("X   X X          X X   X");
+        System.out.println("X   X X          X X   X");
+    }
+}
