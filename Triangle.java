3b77480a8ab07b8572567158c258ad0dd721b4cc
ToamilHere <168946458+Toamil03@users.noreply.github.com>
2025-04-28 23:02:44 -0400

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+// Triangle.java
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.println("    T    ");
+        System.out.println("   TTT   ");
+        System.out.println("  TTTTT  ");
+        System.out.println(" TTTTTTT ");
+        System.out.println("TTTTTTTTT");
+    }
+}
+
